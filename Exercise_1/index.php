d74e8b6a4ccc9b1cd5c55161a1a32474fcbbed9a
Sean Cunningham <47498393+SeanCBlue652@users.noreply.github.com>
2020-11-15 17:28:21 -0600

    Update index.php

--- Exercise_1/index.php
+++ Exercise_1/index.php
@@ -4,7 +4,8 @@ echo "<table border=\"1\" style='border-collapse: collapse'>";
 for ($i = 1; $i < 101; $i++) {
     echo "<tr> \n";
     for ($j = 1; $j < 101; $j++) {
-        echo "<td>($i * $j)</td> \n";
+        $result = $i * $j;
+        echo "<td> $result </td> \n";
     }
     echo "</tr>";
 }
