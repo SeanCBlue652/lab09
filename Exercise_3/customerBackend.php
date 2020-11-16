e417753b769b0bc7e3eb9f10a5f3d7f9880fc792
Sean Cunningham <47498393+SeanCBlue652@users.noreply.github.com>
2020-11-15 20:35:44 -0600

    asdgasdg

--- Exercise_3/customerBackend.php
+++ Exercise_3/customerBackend.php
@@ -8,7 +8,21 @@
     $premium = $_POST["premium"];
     $collectors = $_POST["collectors"];
 
-    $total = $shipping + ($apples * 1) + ($premium * 2) + ($collectors * 10);
+    $total = ($apples * 1) + ($premium * 2) + ($collectors * 10);
+
+    $shipping_cost = 0;
+
+    if ($shipping == "Free 7 Day") {
+        $shipping_cost = 0;
+    }
+    else if($shipping == "$50.00 over night") {
+        $shipping_cost = 50;
+    }
+    else {
+        $shipping_cost = 5;
+    }
+
+    $total = $total + $shipping_cost
 
     echo "<p class='box'>";
         echo "<p class='box'>";
@@ -18,13 +32,13 @@
         echo "<p class='box'>";
             echo '<h3> Purchases: </h3> </br>';
             echo "&emsp;";
-            echo 'Shipping: '.$shipping.'</br>';
+            echo 'Shipping: '.$shipping.' | Subtotal: $'.$shipping_cost.'</br>';
             echo "&emsp;";
-            echo 'Apples: '.$apples.'</br>';
+            echo 'Apples: '.$apples.' | $1 each | Subtotal: $'.$apples.'</br>';
             echo "&emsp;";
-            echo 'Premium Apples: '.$premium.'</br>';
+            echo 'Premium Apples: '.$premium.' | $2 each | Subtotal: $'.($premium * 2).'</br>';
             echo "&emsp;";
-            echo 'Collectors Edition Apples: '.$collectors.'</br>';
+            echo 'Collectors Edition Apples: '.$collectors.' | $1 each | Subtotal: $'.($collectors * 10).'</br>';
         //echo "</p>";
         echo "</br>";
         echo "<p class='box'>";
