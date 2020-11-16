8ec07732bbd17661635139890c76c4ba577afe56
Sean Cunningham <47498393+SeanCBlue652@users.noreply.github.com>
2020-11-15 18:36:29 -0600

    Update Quiz.php

--- Exercise_2/Quiz.php
+++ Exercise_2/Quiz.php
@@ -1,9 +1,10 @@
 <?php
-$q1 = $_POST["q1"]
-$q2 = $_POST["q2"]
-$q3 = $_POST["q3"]
-$q4 = $_POST["q4"]
-$q5 = $_POST["q5"]
+
+$q1 = $_POST["q1"];
+$q2 = $_POST["q2"];
+$q3 = $_POST["q3"];
+$q4 = $_POST["q4"];
+$q5 = $_POST["q5"];
 
 $amount_correct = 0;
 
